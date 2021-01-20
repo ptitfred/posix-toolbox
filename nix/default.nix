8f419cf801e4f1ffc0e24f66466117093bd7c7b1
Frédéric Menou <frederic.menou@gmail.com>
2021-01-20 17:50:04 +0000

    Package git-checkout-log too
    I then generalize the packaging effort and the default.nix is now
    producing a dict of the various scripts packages (for now only
    git-bubbles and git-checkout-log that I, ptitfred, use the more
    frequently on a daily basis).

--- nix/default.nix
+++ nix/default.nix
@@ -1,26 +1,31 @@
 { stdenv, lib, fetchFromGitHub, git, gnused }:
 
-stdenv.mkDerivation rec {
-  name = "git-bubbles";
+let packageScript = script: buildInputs: description:
+      stdenv.mkDerivation rec {
+        name = "posix-toolbox-" + script;
 
-  src = ./..;
+        src = ./..;
 
-  buildInputs = [ git gnused ];
+        inherit buildInputs;
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src/git-bubbles $out/bin/git-bubbles
-  '';
-
-  postFixup =
-    let runtimePath = lib.makeBinPath buildInputs;
-     in ''
-          sed -i "2 i export PATH=${runtimePath}:\$PATH" $out/bin/git-bubbles
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $src/${script} $out/bin/${script}
         '';
 
-  meta = {
-    homepage = "https://github.com/ptitfred/posix-toolbox";
-    description = "A git script to handle pull requests";
-    license = lib.licenses.mit;
-  };
-}
+        postFixup =
+          let runtimePath = lib.makeBinPath buildInputs;
+           in ''
+                sed -i "2 i export PATH=${runtimePath}:\$PATH" $out/bin/${script}
+              '';
+
+        meta = {
+          homepage = "https://github.com/ptitfred/posix-toolbox";
+          inherit description;
+          license = lib.licenses.mit;
+        };
+      };
+
+in { git-bubbles      = packageScript "git-bubbles"      [ git gnused ] "A git script to handle pull requests";
+     git-checkout-log = packageScript "git-checkout-log" [ git ]        "A git script to browser reflog and follow checkouts";
+   }
