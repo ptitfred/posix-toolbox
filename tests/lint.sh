86362f610d2fe6a70cdf7579abbc820e9713b19a
Frédéric Menou <frederic.menou@gmail.com>
2021-01-20 22:22:27 +0000

    Remove unused scripts
    Notably scripts related to android or java development.

--- tests/lint.sh
+++ tests/lint.sh
@@ -3,23 +3,16 @@
 set -e
 
 shellcheck --severity=error \
-  cov \
-  gerrit-cherry-pick \
-  git-ampl \
   git-authors \
   git-backup \
   git-bubbles \
   git-checkout-log \
-  git-deliver \
   git-prd \
   git-pwd \
-  git-resurrect \
   git-rm-others \
   git-search \
   git-short \
   git-std-init \
-  gps-playback \
-  gps-send \
   prd \
   repeat \
   short-path \
