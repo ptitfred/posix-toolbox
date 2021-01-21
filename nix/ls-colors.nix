a1742ebe11d5a7715c7a9c01a9387165af665d9e
Frédéric Menou <frederic.menou@gmail.com>
2021-01-21 20:06:35 +0000

    LS_COLORS shared sourceable script
    Assuming you install ls-colors, you could then patch your bash
    configuration this way:
    
    ```bash
    source $HOME/.nix-profile/share/ls-colors/bash.sh
    ```
    
    I do this via home-manager with `programs.bash.initExtra`.
    
    It's also available for cshell via `$HOME/.nix-profile/share/ls-colors/csh.sh`

---
+++ nix/ls-colors.nix
@@ -0,0 +1,20 @@
+{ stdenv, coreutils, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  name = "ls-colors";
+
+  src = fetchFromGitHub {
+    owner = "trapd00r";
+    repo = "LS_COLORS";
+    rev = "e91cc9cc69f6c4780f03b121bc633569742de7cd";
+    sha256 = "1i2pc9k1g79wvdq3w2h3ikp3s2myalcvwin2l6gmyhz5cn0xjfg8";
+  };
+
+  buildInputs = [ coreutils ];
+
+  installPhase = ''
+    mkdir -p $out/share/ls-colors
+    dircolors -b $src/LS_COLORS > $out/share/ls-colors/bash.sh
+    dircolors -c $src/LS_COLORS > $out/share/ls-colors/csh.sh
+  '';
+}
