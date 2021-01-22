bb91ad77211de4300b41a273b1b6f9b0d683f9e8
Frédéric Menou <frederic.menou@gmail.com>
2021-01-22 13:42:37 +0000

    No need of let..in for cross-dependencies
    Recursive attrsets do the trick (here to have `prd` as a dep of
    `git-prd`, both defined in this expression).

--- nix/scripts.nix
+++ nix/scripts.nix
@@ -1,20 +1,19 @@
 { callPackage, coreutils, findutils, gawk, git, gnugrep, gnused, less, lsof, psmisc, utillinux }:
 
 let packageScript = callPackage ./package.nix {};
-    git-authors      = packageScript "git-authors"      [ coreutils findutils git gnused  ] "A git script to list committers other a commit range";
-    git-bubbles      = packageScript "git-bubbles"      [ coreutils git gnused            ] "A git script to handle pull requests";
-    git-checkout-log = packageScript "git-checkout-log" [ coreutils git gnused less       ] "A git script to browser reflog and follow checkouts";
-    git-prd          = packageScript "git-prd"          [ git prd                         ] "A git script to display the path of the root of a git repository relative to your HOME directory";
-    git-pwd          = packageScript "git-pwd"          [ coreutils git                   ] "A git script to display the path relative to the root of a git repository";
-    git-rm-others    = packageScript "git-rm-others"    [ coreutils findutils git         ] "A git script to clean the working copy from untracked files";
-    git-search       = packageScript "git-search"       [ findutils git gnugrep           ] "A git script to search the diff other a commit range";
-    git-short        = packageScript "git-short"        [ git                             ] "A git script to display short SHA1 of a given commit";
-    git-std-init     = packageScript "git-std-init"     [ coreutils git                   ] "A git script to setup a repository with an initial empty commit and a base and master branches";
-    prd              = packageScript "prd"              [ coreutils                       ] "A script to print the working directory relative to your HOME directory";
-    repeat           = packageScript "repeat"           [ coreutils gnused utillinux      ] "A script to repeat a command some times";
-    short-path       = packageScript "short-path"       [ coreutils gnused                ] "A script to abbreviate every directory unless the last part of a path";
-    wait-tcp         = packageScript "wait-tcp"         [ coreutils gawk gnugrep gnused lsof psmisc ] # FIXME: make buildInputs dependent on the target system (darwin vs linux)
-                         "A script to wait for some server sockets to be opened on a TCP";
-in {
-     inherit git-authors git-bubbles git-checkout-log git-prd git-pwd git-rm-others git-search git-short git-std-init prd repeat short-path wait-tcp;
+in rec {
+     git-authors      = packageScript "git-authors"      [ coreutils findutils git gnused  ] "A git script to list committers other a commit range";
+     git-bubbles      = packageScript "git-bubbles"      [ coreutils git gnused            ] "A git script to handle pull requests";
+     git-checkout-log = packageScript "git-checkout-log" [ coreutils git gnused less       ] "A git script to browser reflog and follow checkouts";
+     git-prd          = packageScript "git-prd"          [ git prd                         ] "A git script to display the path of the root of a git repository relative to your HOME directory";
+     git-pwd          = packageScript "git-pwd"          [ coreutils git                   ] "A git script to display the path relative to the root of a git repository";
+     git-rm-others    = packageScript "git-rm-others"    [ coreutils findutils git         ] "A git script to clean the working copy from untracked files";
+     git-search       = packageScript "git-search"       [ findutils git gnugrep           ] "A git script to search the diff other a commit range";
+     git-short        = packageScript "git-short"        [ git                             ] "A git script to display short SHA1 of a given commit";
+     git-std-init     = packageScript "git-std-init"     [ coreutils git                   ] "A git script to setup a repository with an initial empty commit and a base and master branches";
+     prd              = packageScript "prd"              [ coreutils                       ] "A script to print the working directory relative to your HOME directory";
+     repeat           = packageScript "repeat"           [ coreutils gnused utillinux      ] "A script to repeat a command some times";
+     short-path       = packageScript "short-path"       [ coreutils gnused                ] "A script to abbreviate every directory unless the last part of a path";
+     wait-tcp         = packageScript "wait-tcp"         [ coreutils gawk gnugrep gnused lsof psmisc ] # FIXME: make buildInputs dependent on the target system (darwin vs linux)
+                          "A script to wait for some server sockets to be opened on a TCP";
    }
