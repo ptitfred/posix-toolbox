2ab6463315eb31b0cc743a7c30e80c316eaeeb4b
Frédéric Menou <frederic.menou@gmail.com>
2021-01-22 01:50:04 +0000

    No need to explicitely enumerate the bash scripts

--- tests/lint.sh
+++ tests/lint.sh
@@ -2,17 +2,4 @@
 
 set -e
 
-shellcheck \
-  src/git-authors \
-  src/git-bubbles \
-  src/git-checkout-log \
-  src/git-prd \
-  src/git-pwd \
-  src/git-rm-others \
-  src/git-search \
-  src/git-short \
-  src/git-std-init \
-  src/prd \
-  src/repeat \
-  src/short-path \
-  src/wait-tcp
+shellcheck $(git ls-files src/)
