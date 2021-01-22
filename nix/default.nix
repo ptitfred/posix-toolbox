f813f01522cf0160b19f2ecfb03e4b215b8ea12f
Frédéric Menou <frederic.menou@gmail.com>
2021-01-22 14:40:56 +0000

    Nix: package git-ps1 as a shared script to source
    From you bash configuration:
    
    ```bash
    source ~/.nix-profile/share/posix-toolbox/git-ps1
    ```

--- nix/default.nix
+++ nix/default.nix
@@ -2,4 +2,10 @@
 
 let scripts  = callPackage ./scripts.nix   {};
     lsColors = callPackage ./ls-colors.nix {};
-in scripts // lsColors
+    git-ps1  = callPackage ./git-ps1.nix   {
+      git-pwd    = scripts.git-pwd;
+      git-prd    = scripts.git-prd;
+      prd        = scripts.prd;
+      short-path = scripts.short-path;
+    };
+in scripts // lsColors // git-ps1
