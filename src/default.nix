d4e240ed06b1f879751960ab8f0b03fff240b6a1
Frédéric Menou <frederic.menou@gmail.com>
2024-10-13 12:41:20 +0200

    i3-screen-locker no longer used

--- src/default.nix
+++ src/default.nix
@@ -1,7 +1,7 @@
 { callPackage, trapd00r-ls-colors
 , lib, stdenv, makeWrapper
-, coreutils, findutils, gawk, git, gnugrep, gnused, i3lock, imagemagick, less
-, lsof, psmisc, scrot, tree, utillinux }:
+, coreutils, findutils, gawk, git, gnugrep, gnused, less
+, lsof, psmisc, tree, utillinux }:
 
 let packageScript =
       script: inputs: description:
@@ -38,7 +38,6 @@ rec {
    git-short        = packageScript "git-short"        [ git                                       ] "A git script to display short SHA1 of a given commit";
    git-std-init     = packageScript "git-std-init"     [ coreutils git                             ] "A git script to setup a repository with an initial empty commit and a base and master branches";
    git-tree         = packageScript "git-tree"         [ coreutils git tree                        ] "A git script to tree files handled by git";
-   i3-screen-locker = packageScript "i3-screen-locker" [ coreutils i3lock imagemagick scrot        ] "A variant of i3lock that take a screenshot to use as background of the lock screen";
    prd              = packageScript "prd"              [ coreutils                                 ] "A script to print the working directory relative to your HOME directory";
    repeat           = packageScript "repeat"           [ coreutils gnused utillinux                ] "A script to repeat a command some times";
    short-path       = packageScript "short-path"       [ coreutils gnused                          ] "A script to abbreviate every directory unless the last part of a path";
